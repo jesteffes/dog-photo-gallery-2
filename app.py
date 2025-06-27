913d6d5e966989520605c5839dd4a191d788066e
Jenna Steffes <168142203+jesteffes@users.noreply.github.com>
2025-06-27 15:42:11 -0700

    Initial commit

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template, request
+
+import requests
+
+#imports a dictionary of data from dog_breeds.py and "prettifies", or styles, the dog names when they appear in the HTML page
+from dog_breeds import prettify_dog_breed
+
+# Initialize the Flask application
+app = Flask(__name__)
+
+#function adds a dash in the URL between breed names with multiple words like miniature poodle
+def check_breed(breed):
+  return "/".join(breed.split("-"))
+
+@app.route("/", methods=["GET", "POST"])
+def dog_image_gallery():
+  errors = []
+  if request.method == "POST":
+     breed = request.form.get("breed")
+  return render_template("dogs.html")
+
+
+app.debug = True
+
+# Run the flask server
+if __name__ == "__main__":
+    app.run()
