746516a5c2cd8b22b6d43bbedcf55f9b15aef85a
Jenna Steffes <168142203+jesteffes@users.noreply.github.com>
2025-07-10 17:05:26 -0700

    Updated app with jinja placeholders

--- app.py
+++ app.py
@@ -12,18 +12,19 @@ app = Flask(__name__)
 def check_breed(breed):
   return "/".join(breed.split("-"))
 
-@app.route("/", methods=["GET", "POST"])
+@app.route("/", methods=["GET","POST"])
 def dog_image_gallery():
   errors = []
   if request.method == "POST":
      breed = request.form.get("breed")
      if not breed:
         errors.append("Oops! Please choose a breed.")
-        if breed:
-           response = requests.get("https://dog.ceo/api/breed/" + check_breed(breed) + "/images/random/30")
-           data = response.json()
-           dog_images = data["message"]
-  return render_template("dogs.html")
+     if breed:
+        response = requests.get("https://dog.ceo/api/breed/" + check_breed(breed) + "/images/random/30")
+        data = response.json()
+        dog_images = data["message"]
+        return render_template("dogs.html", images=dog_images, breed=prettify_dog_breed(breed), errors=[])        
+  return render_template("dogs.html", images=[], breed="", errors=errors)
 
 
 app.debug = True
